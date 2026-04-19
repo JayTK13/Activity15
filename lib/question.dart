6d4bb31ad0085e63618532f84be820ff635b4ea6
Jaden Woody <jwoody6@student.gsu.edu>
2026-04-19 00:54:17 -0400

    created question model with JSON and shuffled answer options

---
+++ lib/question.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+class Question {
+  final String question;
+  final String correctAnswer;
+  final List<String> options;
+
+  Question({
+    required this.question,
+    required this.correctAnswer,
+    required this.options,
+  });
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    List<String> allOptions = List<String>.from(json['incorrect_answers']);
+
+    allOptions.add(json['correct_answer']);
+    allOptions.shuffle(Random());
+
+    return Question(
+      question: json['question'],
+      correctAnswer: json['correct_answer'],
+      options: allOptions,
+    );
+  }
+}
